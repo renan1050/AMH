9a887587b063eef948bbdc915928771c10b8ef91
renan1050 <renan.campos@live.com>
2016-04-04 12:46:35 -0300

    Layout

--- Allianz/AllianzMaintenanceHelper/MainWindow.xaml.cs
+++ Allianz/AllianzMaintenanceHelper/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InterfaceBase;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,7 +20,7 @@ namespace AllianzMaintenanceHelper
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
+    {        
         public MainWindow()
         {  
             InitializeComponent();
---
+++ Allianz/BusinessRules/InterfaceBase/WPFExtension.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceBase
+{
+    public class WPFExtension
+    {
+        public static readonly DependencyProperty RelativeFieldCodeProperty =
+        DependencyProperty.RegisterAttached("RelativeFieldCode", typeof(double), typeof(WPFExtension), new PropertyMetadata(default(string)));
+
+        public static void SetRelativeFieldCode(UIElement element, double value)
+        {
+            element.SetValue(RelativeFieldCodeProperty, value);
+        }
+
+        public static string GetRelativeFieldCode(UIElement element)
+        {
+            return (string)element.GetValue(RelativeFieldCodeProperty);
+        }
+    }
+}
