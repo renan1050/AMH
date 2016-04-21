71a51ddcde7f6815b1914a8f9e1b939052d2a16a
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2016-04-21 14:26:32 -0300

    Cadastro de veiculo (Interface)

---
+++ Allianz/AllianzMaintenanceHelper/CadastroVeiculo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AllianzMaintenanceHelper
+{
+    /// <summary>
+    /// Interaction logic for CadastroVeiculo.xaml
+    /// </summary>
+    public partial class CadastroVeiculo : Window
+    {
+        public CadastroVeiculo()
+        {
+            InitializeComponent();
+        }
+    }
+}
