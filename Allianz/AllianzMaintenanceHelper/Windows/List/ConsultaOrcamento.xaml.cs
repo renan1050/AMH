079368989c916e9cf4df1a2334e888198a01ec4d
guigax <guilherme.fonseca.eu@gmail.com>
2016-04-25 10:55:35 -0300

    Cadastro e consulta de orçamento

---
+++ Allianz/AllianzMaintenanceHelper/Windows/List/ConsultaOrcamento.xaml.cs
@@ -0,0 +1,28 @@
+﻿using InterfaceBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AllianzMaintenanceHelper
+{
+    /// <summary>
+    /// Interaction logic for ConsultaOrcamento.xaml
+    /// </summary>
+    public partial class ConsultaOrcamento : Window
+    {
+        public ConsultaOrcamento()
+        {
+            InitializeComponent();
+        }
+    }
+}
