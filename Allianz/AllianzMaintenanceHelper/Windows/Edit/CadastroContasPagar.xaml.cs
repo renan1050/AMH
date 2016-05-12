03fae557cfc2c8142497fb892d03eff73c10cdf3
Vinicius Pinheiro <vinicius_bit@yahoo.com.br>
2016-05-12 09:42:20 -0300

    Telas

---
+++ Allianz/AllianzMaintenanceHelper/Windows/Edit/CadastroContasPagar.xaml.cs
@@ -0,0 +1,22 @@
+﻿using BusinessRules.DatabaseBase.Classes;
+using BusinessRules.DatabaseBase.Model;
+using InterfaceBase;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace AllianzMaintenanceHelper.Windows.Edit
+{
+    /// <summary>
+    /// Interaction logic for CadastroContasPagar.xaml
+    /// </summary>
+    public partial class CadastroContasPagar : Window
+    {
+        public CadastroContasPagar()
+        {
+            InitializeComponent();
+        }
+    }
+}
